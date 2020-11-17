7ef584fdbe87dbebf286103559e16f7bde3c0dc0
JarudatKMITL <jarudat.ja@gmail.com>
2020-11-18 00:54:08 +0700

    debugging

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -4,7 +4,10 @@ var modbusModel=db.model('modbus');
 
 var render=(req,res)=>{
     modbusModel.find({},(err,data)=>{
-	    if(!err)res.render('index',{tempData: data});
+	    if(!err){
+            console.log(data);
+            res.render('index',{tempData: data});
+        }
     });
 }
 
